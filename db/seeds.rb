61a41798b1a2ddd6b724bf2e04bf1874e7a27f99
Kenji kojima <k.kojima09211986@gmail.com>
2019-06-12 22:12:14 +0900

    complete_ransack

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: '斎藤', age: 34)
+User.create!(name: '小島', age: 23)
+User.create!(name: '後藤', age: 30)
+User.create!(name: '小林', age: 34)
+User.create!(name: 'した林', age: 34)
+User.create!(name: '藤田', age: 239)
